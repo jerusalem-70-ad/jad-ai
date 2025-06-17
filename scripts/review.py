1d1d159741836c81bb03d4c525bbb496e0085587
csae8092 <p.andorfer@gmail.com>
2025-06-17 14:17:05 +0200

    feat: better review

--- scripts/review.py
+++ scripts/review.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
 import requests
+import os
 import pandas as pd
-from config import PASSAGES_URL
+from config import PASSAGES_URL, OUT_DIR
+from acdh_bible_pyutils import get_book_from_bibl_ref
+from csae_pyutils import save_json, load_json
 
 
 URL = PASSAGES_URL
@@ -43,3 +47,68 @@ stats = {
 
 for key, value in stats.items():
     print(f"{key.replace('_', ' ').title()}: {value}")
+
+
+URL = PASSAGES_URL
+APP_URL = "https://jerusalem-70-ad.github.io/jad-astro/passages/"
+source_data = requests.get(URL).json()
+
+manual_refs = defaultdict(list)
+for x in source_data.values():
+    bibl_refs = x["biblical_references"]
+    if bibl_refs:
+        for y in bibl_refs:
+            book_name = get_book_from_bibl_ref(y["value"])["order"]
+            manual_refs[x["jad_id"]].append(book_name)
+save_json(manual_refs, "manual_refs.json")
+
+source_data = load_json(os.path.join(OUT_DIR, "all_in_one.json"))
+
+ai_refs = defaultdict(list)
+for key, value in source_data.items():
+    for y in value:
+        try:
+            bibl_ref = y["bibl"]
+        except KeyError:
+            continue
+        book = get_book_from_bibl_ref(bibl_ref)["order"]
+        ai_refs[key].append(book)
+save_json(ai_refs, "ai_refs.json")
+
+data = {}
+for key, value in ai_refs.items():
+    index_id = int(key.split("__")[-1])
+    data[index_id] = {"jad_id": key, "ai": sorted(value), "nr_ai": len(value)}
+    try:
+        data[index_id]["manual"] = sorted(manual_refs[key])
+    except KeyError:
+        data[index_id]["manual"] = []
+    data[index_id]["nr_manual"] = len(data[index_id]["manual"])
+
+
+df = pd.DataFrame.from_dict(data, orient="index").sort_index()
+df.head()
+df = df[df["nr_manual"] > 0]
+df.head()
+
+df["nr_match"] = df["nr_ai"] == df["nr_manual"]
+
+df
+
+df["full_match"] = df.apply(lambda row: row["ai"] == row["manual"], axis=1)
+
+df
+
+df["partial_match"] = df.apply(
+    lambda row: bool(set(row["ai"]) & set(row["manual"])), axis=1
+)
+
+total_rows = len(df)
+stats = {
+    "nr_match": df["nr_match"].sum(),
+    "full_match": df["full_match"].sum(),
+    "partial_match": df["partial_match"].sum(),
+}
+print("### BOOKS ###")
+for col, val in stats.items():
+    print(f"{col}: {val} out of {total_rows} rows ({val/total_rows*100:.1f}%)")
