f03592acd3fede78784cd9886eec61ab94f8d231
csae8092 <p.andorfer@gmail.com>
2025-06-20 08:51:25 +0200

    feat: human vs. ai review

--- scripts/review_ai_vs_human.py
+++ scripts/review_ai_vs_human.py
@@ -1,9 +1,8 @@
-from collections import defaultdict
 import requests
 import os
 import pandas as pd
 from config import PASSAGES_URL, OUT_DIR
-from acdh_bible_pyutils import get_book_from_bibl_ref
+from acdh_bible_pyutils import normalize_bible_refs
 from csae_pyutils import save_json, load_json
 
 
@@ -44,69 +43,128 @@ stats = {
     "longest_passage_word_count": int(df["word_count"].max()),
     "shortest_passage_word_count": int(df["word_count"].min()),
 }
-print("\n\n### Passages and words ###\n")
+print("\n### Passages and words ###\n")
 for key, value in stats.items():
     print(f"{key.replace('_', ' ').title()}: {value}")
 
-
-URL = PASSAGES_URL
-APP_URL = "https://jerusalem-70-ad.github.io/jad-astro/passages/"
-source_data = requests.get(URL).json()
-
-manual_refs = defaultdict(list)
+manual_refs = list()
 for x in source_data.values():
-    bibl_refs = x["biblical_references"]
-    if bibl_refs:
-        for y in bibl_refs:
-            book_name = get_book_from_bibl_ref(y["value"])["order"]
-            manual_refs[x["jad_id"]].append(book_name)
+    # we need to remove passages without text paragraphs because
+    # there are passages without texts but with biblical references
+    if x["text_paragraph"]:
+        bibl_refs = x["biblical_references"]
+        jad_id = x["jad_id"]
+        num = int(jad_id.split("__")[-1])
+        if bibl_refs:
+            item = {
+                "id": num,
+                "jad_id": jad_id,
+                "books": [],
+                "chapters": [],
+                "verses": [],
+            }
+            for y in bibl_refs:
+                books = item["books"]
+                chapters = item["chapters"]
+                verses = item["verses"]
+                normalized_ref = normalize_bible_refs(y["value"])
+                books.append(normalized_ref["order"])
+                chapters.append(normalized_ref["chapter"])
+                verse_string = f'{normalized_ref["title_lat"]} {normalized_ref["chapter"]}:{normalized_ref["verse_start"]}'  # noqa: E501
+                verses.append(verse_string)
+        manual_refs.append(item)
 save_json(manual_refs, "manual_refs.json")
 
 source_data = load_json(os.path.join(OUT_DIR, "all_in_one.json"))
 
-ai_refs = defaultdict(list)
+ai_refs = list()
 for key, value in source_data.items():
+    jad_id = key
+    num = int(jad_id.split("__")[-1])
+    item = {
+        "id": num,
+        "jad_id": jad_id,
+        "books": [],
+        "chapters": [],
+        "verses": [],
+    }
     for y in value:
         try:
             bibl_ref = y["bibl"]
         except (KeyError, TypeError):
             continue
-        book = get_book_from_bibl_ref(bibl_ref)["order"]
-        ai_refs[key].append(book)
+        books = item["books"]
+        chapters = item["chapters"]
+        verses = item["verses"]
+        normalized_ref = normalize_bible_refs(y["bibl"])
+        books.append(normalized_ref["order"])
+        chapters.append(normalized_ref["chapter"])
+        verse_string = f'{normalized_ref["title_lat"]} {normalized_ref["chapter"]}:{normalized_ref["verse_start"]}'
+        verses.append(verse_string)
+    for key, value in item.items():
+        if isinstance(value, list):
+            item[key] = sorted(value)
+    ai_refs.append(item)
 save_json(ai_refs, "ai_refs.json")
 
-data = {}
-for key, value in ai_refs.items():
-    index_id = int(key.split("__")[-1])
-    data[index_id] = {"jad_id": key, "ai": sorted(value), "nr_ai": len(value)}
-    try:
-        data[index_id]["manual"] = sorted(manual_refs[key])
-    except KeyError:
-        data[index_id]["manual"] = []
-    data[index_id]["nr_manual"] = len(data[index_id]["manual"])
+df_manual = pd.DataFrame(manual_refs).sort_values("id")
+df_manual = df_manual.drop_duplicates(subset=["jad_id"], keep="first")
 
+df_ai = pd.DataFrame(ai_refs).sort_values("id")
+df_ai = df_ai.drop_duplicates(subset=["jad_id"], keep="first")
+df = df_manual.merge(df_ai, on="jad_id", how="left", suffixes=("_manual", "_ai"))
+df = df.drop(columns=["id_ai"])
+# Fill NaN values with empty lists for columns ending with "_ai"
+ai_columns = [col for col in df.columns if col.endswith("_ai")]
+for col in ai_columns:
+    df[col] = df[col].apply(lambda x: [] if isinstance(x, float) and pd.isna(x) else x)
 
-df = pd.DataFrame.from_dict(data, orient="index").sort_index()
-df = df[df["nr_manual"] > 0]
-df["nr_match"] = df["nr_ai"] == df["nr_manual"]
-df["full_match"] = df.apply(lambda row: row["ai"] == row["manual"], axis=1)
-df["partial_match"] = df.apply(
-    lambda row: bool(set(row["ai"]) & set(row["manual"])), axis=1
+df["full_match_book"] = df.apply(
+    lambda row: row["books_ai"] == row["books_manual"], axis=1
 )
-df["all_found"] = df.apply(
-    lambda row: set(row["manual"]).issubset(set(row["ai"])), axis=1
+df["full_match_chapter"] = df.apply(
+    lambda row: row["chapters_ai"] == row["chapters_manual"], axis=1
 )
+df["full_match_verse"] = df.apply(
+    lambda row: row["verses_ai"] == row["verses_manual"], axis=1
+)
+
+df["partial_match_book"] = df.apply(
+    lambda row: bool(set(row["books_ai"]) & set(row["books_manual"])), axis=1
+)
+df["partial_match_chapter"] = df.apply(
+    lambda row: bool(set(row["chapters_ai"]) & set(row["chapters_manual"])), axis=1
+)
+df["partial_match_verse"] = df.apply(
+    lambda row: bool(set(row["verses_ai"]) & set(row["verses_manual"])), axis=1
+)
+df["all_found_books"] = df.apply(
+    lambda row: set(row["books_manual"]).issubset(set(row["books_ai"])), axis=1
+)
+df["all_found_chapters"] = df.apply(
+    lambda row: set(row["chapters_manual"]).issubset(set(row["chapters_ai"])), axis=1
+)
+df["all_found_verses"] = df.apply(
+    lambda row: set(row["verses_manual"]).issubset(set(row["verses_ai"])), axis=1
+)
+
 total_rows = len(df)
 stats = {
-    "partial_match": df["partial_match"].sum(),
-    "all_found": df["all_found"].sum(),
-    "nr_match": df["nr_match"].sum(),
-    "full_match": df["full_match"].sum(),
+    "partial_match_book": df["partial_match_book"].sum(),
+    "partial_match_chapter": df["partial_match_chapter"].sum(),
+    "partial_match_verse": df["partial_match_verse"].sum(),
+    "all_found_books": df["all_found_books"].sum(),
+    "all_found_chapters": df["all_found_chapters"].sum(),
+    "all_found_verses": df["all_found_verses"].sum(),
+    "full_match_book": df["full_match_book"].sum(),
+    "full_match_chapter": df["full_match_chapter"].sum(),
+    "full_match_verse": df["full_match_verse"].sum(),
 }
-print("\n\n### Books ###\n")
+print("\n### Human vs AI ###\n")
 for col, val in stats.items():
-    print(f"{col}: {val} out of {total_rows} rows ({val/total_rows*100:.1f}%)")
+    print(
+        f"{col.replace('_', ' ').title()}: {val} out of {total_rows} passages ({val/total_rows*100:.1f}%)"
+    )
 
-
-print("\n\n### Chapters ###\n")
-print("Todo: Implement chapter statistics")
+print("\n### Done ###\n\n")
+df.to_csv(os.path.join(OUT_DIR, "human_vs_ai.csv"), index=False)
