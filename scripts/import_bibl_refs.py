f8b292e6dbf6e477810ec96c9f476adf911de68a
csae8092 <p.andorfer@gmail.com>
2026-01-29 16:37:12 +0100

    fix: filter out non exisiting ids before updating baserow

--- scripts/import_bibl_refs.py
+++ scripts/import_bibl_refs.py
@@ -1,15 +1,23 @@
 import os
 import json
+import requests
 from acdh_baserow_pyutils import BaseRowClient
+from config import PASSAGES_URL
 
-
+print("foo")
 with open(os.path.join("out", "all_in_one.json"), "r", encoding="utf-8") as fp:
     data = json.load(fp)
-
+print(f"loading passages from {PASSAGES_URL}")
+passages = requests.get(PASSAGES_URL).json()
+ids = [int(x) for x in passages.keys()]
+print("done loading")
 payload = []
 for key, value in data.items():
     item = {}
     item["id"] = int(key.split("__")[-1])
+    if item["id"] not in ids:
+        print(f"{item['id']} does not exist any more")
+        continue
     try:
         item["ai_bible_refs"] = "; ".join(set([x["bibl"] for x in value]))
     except TypeError:
